9d8896ca61d6fe2ca3749503f1b9b1edb27d1448
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-11-07 11:40:22 +0200

    init: set up project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TracksModule } from './tracks/tracks.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TracksModule,
+    FavoritesModule,
+    ArtistsModule,
+    AlbumsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
