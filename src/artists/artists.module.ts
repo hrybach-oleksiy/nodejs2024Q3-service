c8bfeee98ac0c8ecef47995fc1bfbce561893356
Hrybach Oleksiy <a.a.gribach@gmail.com>
2024-11-11 20:08:25 +0200

    feat: add swagger

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -3,10 +3,9 @@ import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
 import { TracksModule } from 'src/tracks/tracks.module';
 import { AlbumsModule } from 'src/albums/albums.module';
-import { FavoritesModule } from 'src/favorites/favorites.module';
 
 @Module({
-  imports: [TracksModule, AlbumsModule, FavoritesModule],
+  imports: [TracksModule, AlbumsModule],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,19 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as jsYaml from 'js-yaml';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const filePath = path.resolve(process.cwd(), 'doc', 'api.yaml');
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const swaggerObject = jsYaml.load(fileContent) as OpenAPIObject;
+  SwaggerModule.setup('/doc', app, swaggerObject);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
